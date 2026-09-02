2a8245f52a2b0b213408bb3f6a3e8cba23109958
Jose Manuel Reina Nogales <jose.reina.nogales@gmail.com>
2026-09-02 21:00:14 +0200

    Fix external tables

--- terraform/main.tf
+++ terraform/main.tf
@@ -99,7 +99,7 @@ resource "google_bigquery_table" "ext_binance_raw" {
 
   external_data_configuration {
     autodetect    = false
-    source_format = "JSON"
+    source_format = "NEWLINE_DELIMITED_JSON"
     source_uris   = ["gs://${google_storage_bucket.raw_bucket.name}/crypto/binance/*/*/*/*.json"]
 
     schema = <<EOF
